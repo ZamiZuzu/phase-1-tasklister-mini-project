52f2218d0eed96a334461fda2bc810243ac372f9
Christopher Valle <chrisbv@live.com>
2021-10-18 23:59:00 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        buildToDo(e.target.querySelector('#new-task-description').value)
+        form.reset()
 });
+})
+
+function buildToDo(todo){
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  
+  p.textContent = `${todo} `;
+  btn.textContent = ' x';
+  btn.addEventListener('click', handleDelete)
+
+  p.appendChild(btn);
+  document.querySelector('#tasks').appendChild(p);
+
+
+ 
+  // document.querySelector
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
+
+
